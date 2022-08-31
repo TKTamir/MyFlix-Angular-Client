58ae8512070355de9641d7bfec94d9769161868b
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-08-31 12:20:39 +0200

    Create registerUser function

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -16,9 +16,42 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  @input() userData = { Username: '', Password: '', Email: '', Birthday: ''};
+
+  /**
+   * 
+   * @param fetchApiData 
+   * @param dilaogReg 
+   * @param snackBar 
+   */
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dilaogReg: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  //Function that's responsible for sending the form inputs to the backend
+  /**
+   * Function for sending the form inputs to the backend to create a new user
+   * @returns alert indicating a successful registration or an error
+   */
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+    // Logic for a successful user registration goes here(to be implemented)
+    console.log(result)
+    this.dialogRef.close(); //This will close the modal on success
+    this.snackBar.open('User registration successful', 'OK', {
+      duration: 2000
+    });
+    }, (result) => {
+      this.snackBar.open('User registration successful', 'OK', {
+        duration: 200
+      });
+    });
+  }
+  
 }
+
