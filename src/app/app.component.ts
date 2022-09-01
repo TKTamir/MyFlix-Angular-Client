dd43a9e7bde27a046cac8fd1dd3bc7f4b5baabd4
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-01 14:18:59 +0200

    Setup Movie-Card-Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,24 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-    //Function that opens the dialog when the signup button is clicked
+    //Function that opens the registration dialog when the signup button is clicked
     openUserRegistrationDialog(): void {
       this.dialog.open(UserRegistrationFormComponent, {
         //Assigning the dialog a width
         width: '280px'
       }); 
     }
-
+    //Function that opens the login dialog when the signup button is clicked
     openUserLoginDialog(): void {
       this.dialog.open(UserLoginFormComponent, {
         //Assigning the dialog a width
         width: '280px'
       }); 
+    }
+    //Function that opens the movies dialog after the button is clicked
+    openMoviesDialog(): void {
+      this.dialog.open(MovieCardComponent, {
+        width: '500px'
+      });
     }
 }
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent {
+  movies: any[] = []; //Movies returned from the API will be stored here, similiar to State in React
+  constructor(public fetchApiData: FetchApiDataService) { } //FetchApiDatService calls the api and returns movies.
 
-  constructor() { }
-
-  ngOnInit(): void {
+  //Lifecycle hook that's being called with the getMovies function after Angular finishes creating the component
+ngOnInit(): void {
+  this.getMovies();
+}
+//Function calls the api with FetchApiData and getAllMovies and returns the movies in the state of this.movies
+getMovies(): void {
+  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
   }
-
 }
