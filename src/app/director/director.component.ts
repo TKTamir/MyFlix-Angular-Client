6259c67b4eb4499c31e1670e82d0375bed13835e
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-02 16:25:01 +0200

    Setup Director component

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string,
+      Bio: string,
+      Birth: Date,
+    }
+  ) { }
 
   ngOnInit(): void {
   }
