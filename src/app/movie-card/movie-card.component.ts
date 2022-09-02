e3ad474f3defbec70d17540b4f723b14ab6bc39b
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-02 15:52:23 +0200

    Write functions for Movie-Card methods

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
 
+import { GenreComponent } from '../genre/genre.component';
+import { DirectorComponent } from '../director/director.component';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar
 
 @Component({
   selector: 'app-movie-card',
@@ -11,13 +17,17 @@ export class MovieCardComponent {
   movies: any[] = []; //Movies returned from the API will be stored here, similiar to State in React
   favoriteMovies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { } //FetchApiDatService calls the api and returns movies.
+  constructor(
+    public fetchApiData: FetchApiDataService, //FetchApiDatService calls the api and returns movies.
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+    ) { } 
 
   //Lifecycle hook that's being called with the getMovies function after Angular finishes creating the component
 ngOnInit(): void {
   this.getMovies();
 }
-//Function calls the api with FetchApiData and getAllMovies and returns the movies in the state of this.movies
+//Function calls the api and returns the movies in the state of this.movies
 getMovies(): void {
   this.fetchApiData.getAllMovies().subscribe((response: any) => {
       this.movies = response;
@@ -25,7 +35,7 @@ getMovies(): void {
       return this.movies;
     });
   }
-
+// Function calls the api and returns the FavoriteMovies array
   getFavoriteMovies(): void {
     this.fetchApiData.getFavorite().subscribe((response: any) => {
       this.favoriteMovies = response;
@@ -33,6 +43,66 @@ getMovies(): void {
       return this.favoriteMovies;
     });
   }
+
+// Function checks if a movie is included in the FavoriteMovies array
+isFav(id: string): boolean {
+  return this.favoriteMovies.includes(id)
+}
+
+// Function opens the Genre dialog and displays its details
+openGenreDialog(name: string, description: string): void {
+  this.dialog.open(GenreComponent, {
+    data: {
+      Name: name,
+      Description: description,
+    },
+    width: '400px'
+  });
+}
+
+//Function opens the Director dialog and displays its details
+openDirectorDialog(name: string, bio: string, birth: Date, death: Date): void {
+  this.dialog.open(DirectorComponent, {
+    data: {
+      Name: name,
+      Bio: bio,
+      Birth: birth,
+      Death: death,
+    },
+    // Assign dialog width
+    width: '400px'
+  });
+}
+
+//Function to open the Synopsis dialog and show the movies details
+openSynopsisDialog(title: string, description: string): void {
+  this.dialog.open(SynopsisComponent, {
+    data: {
+      Title: title,
+      Description: description,
+    },
+    
+    width: '400px'
+  });
+}
+
+//Function to add a movie to users favorites
+addToFavoriteMovies(id: string): void {
+  console.log(id);
+  this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
+    console.log(result);
+    this.ngOnInit();
+  })
+}
+
+//Function to remove a movie from users favorites
+removeFromFavoriteMovies(id: string): void {
+  console.log(id);
+  this.fetchApiData.removeFavoriteMovie(id).subscribe((result) => {
+    console.log(result);
+    this.ngOnInit();
+  })
+}
 
 
 }
