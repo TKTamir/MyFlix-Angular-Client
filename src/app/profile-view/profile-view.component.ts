735d242d09c1afeb95eb391cc0506c13c9b68aa0
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-09-02 12:44:57 +0200

    Crate Profile-View component

---
+++ src/app/profile-view/profile-view.component.ts
@@ -0,0 +1,31 @@
+// src/app/user-registration-form/user-registration-form.component.ts
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+//This import is used to import Angulars Router
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-view',
+  templateUrl: './profile-view.component.html',
+  styleUrls: ['./profile-view.component.scss']
+})
+export class ProfileViewComponent implements OnInit {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef,
+    public snackBar: MatSnackBar,
+    private router: Router
+    ) { }
+  ngOnInit(): void {
+  }
+
+}
